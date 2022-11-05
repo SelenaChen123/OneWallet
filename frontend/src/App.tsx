cbec34349c4c375d7d1416c9ff0d3a34121fc65d
piepielovers <tboywu@gmail.com>
2022-11-05 16:39:47 -0400

    added balance widget to app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
 import logo from './logo.svg';
-import Bills from './widgets/Bills'
+import Balance from './widgets/Balance';
+import Bills from './widgets/Bills';
 
 function App() {
   return (
     <div className="center">
+      <Balance accounts={null}/>
       <Bills />
     </div>
   );
