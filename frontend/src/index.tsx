a75d17d8e43a3e4b32b45418a36cdde27b8e3c78
Alex Snezhko <alexsnezhko89@gmail.com>
2022-11-05 14:22:30 -0400

    react router skeleton

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Login } from './Login';
+import { UserInfo } from './UserInfo';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserInfo />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-8s4z77rcverlv0at.us.auth0.com"
+      clientId="LmIU1umYzy70HALfwCbYPzaRF08oCg1x"
+      redirectUri={window.location.origin}
+      audience="https://hacknc2022ast-api"
+      scope="read:bankinfo"
+    >
+      <RouterProvider router={router} />
+    </Auth0Provider>,
   </React.StrictMode>
 );
 
